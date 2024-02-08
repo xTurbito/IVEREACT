1c1de301f13731dd81e5396b89a9a7065c2a66c2
xTurbito <xTurbito>
2024-02-08 00:32:41 -0600

    cambios

---
+++ ive/src/users/ShowUser.js
@@ -0,0 +1,6 @@
+//Axios nos permite trabajar con protocolos http 
+import axios from 'axios'
+//Biblioteca de hooks que hace los componenetes sean funcionaes y trabajar con APIS
+import {useState, useEffect } from 'react';
+//Link nos ayuda a crear enlaces de navegacion entre diferentes rutas
+import {Link } from 'react-router-dom';
